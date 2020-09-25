c3a5df11e6114022bd052c9a54270d0a2dfbc344
Satyam Kumar <io.satyamtg@gmail.com>
2020-09-25 10:20:38 +0530

    Fixes #153 - Enable mobile navigation

--- openedx2zim/templates/assets/mooc.js
+++ openedx2zim/templates/assets/mooc.js
@@ -1,6 +1,6 @@
 function show_sidemenu(){
-  var e=document.getElementsByClassName("course-index")[0];
-  var course_content = document.getElementById("course-content");
+  var e=document.getElementsByClassName("zim-course-index")[0];
+  var course_content = document.getElementById("zim-course-content");
   if(e.style.display == 'none' || e.style.display == '') {
     e.style.display = 'block';
     course_content.style.display = 'none';
@@ -15,8 +15,8 @@ function show_sidemenu(){
 }
 
 function show_pagemobilemenu(){
-  var e=document.getElementsByClassName("courseware")[0];
-  var course_content = document.getElementById("course-content");
+  var e=document.getElementsByClassName("zim-courseware")[0];
+  var course_content = document.getElementById("zim-course-content");
   if(e.style.display == 'none' || e.style.display == '') {
     e.style.display = 'block';
     //course_content.style.display = 'none';
